83ada47bea19946361fad8fadfa938627ca96e29
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-11-03 15:24:13 +0530

    chapter 4 start

--- FreeCodeCamp/3. escape sequences.js
+++ FreeCodeCamp/3. escape sequences.js
@@ -1,1 +1,15 @@
 console.log("It\'s \"Sreelakshmi\'s Partner\"");
+
+// defining the string inside '' : one can avoid the use of \" to represent qoutes
+
+/*
+\' single 
+\" double
+\\ backslash
+\n new line
+\r carriage return
+\t tab
+\b backspace
+\f form feed 
+*/
+
---
+++ FreeCodeCamp/4. Strings.js
@@ -0,0 +1,9 @@
+// string concatenation using '+' operator
+console.log("Hello, " + "Sreelakshmi"); // Hello, Sre
+var name = "Sreelakshmi ";
+name += "K ";
+lastname = "Stark"
+console.log(name + lastname);
+
+// string length
+console.log(name.length);
