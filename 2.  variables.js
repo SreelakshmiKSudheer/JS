2b43560e4fa5de70a13d9c2cc0431fc67981a2bb
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-11-03 12:55:43 +0530

    chapter 1 done

---
+++ 2.  variables.js
@@ -0,0 +1,4 @@
+var a; // declare
+var b = 2; // initialize
+
+a = 7; // assign
---
+++ FreeCodeCamp/1. Comments and Variables.js
@@ -0,0 +1,8 @@
+// inline or single line comment
+/*
+ multi line comment
+*/
+
+var myName = "Sreelakshmi";  // variable
+let myAge = 25; // within the declared block or segment use
+const pi = 3.14; // as constant
