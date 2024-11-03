8a0f7a0fdd194a13cb1d6fd252cffae3a898e2a9
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-11-03 15:30:00 +0530

    chapter 4

--- FreeCodeCamp/4. Strings.js
+++ FreeCodeCamp/4. Strings.js
@@ -2,9 +2,11 @@
 console.log("Hello, " + "Sreelakshmi"); // Hello, Sre
 var name = "Sreelakshmi ";
 name += "K ";
-lastname = "Stark"
+lastname = "Start"
 console.log(name + lastname);
 
 // string length
 console.log(name.length);
 console.log(name[0]);      // character indexing in string
+lastname[4] = 'k';
+console.log(lastname);
