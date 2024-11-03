0f3a6eafa9bd2d398d050990868fe33758e800a3
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-11-03 13:31:10 +0530

    chapter 3

--- FreeCodeCamp/2.  variables.js
+++ FreeCodeCamp/2.  variables.js
@@ -2,3 +2,31 @@ var a; // declare
 var b = 2; // initialize
 
 a = 7; // assign
+console.log("a = ", a);
+console.log("b = ", b);
+
+// arithmetic
+var result;
+result  = a + b; // add
+console.log("a + b = ", result);
+
+result  = a - b; // sub
+console.log("a - b = ", result);
+
+result  = a * b; // mul
+console.log("a * b = ", result);
+
+result  = a / b; // div
+console.log("a / b = ", result);
+
+result  = a % b; // div
+console.log("a mod b = ", result);
+
+// pre
+console.log(++a);
+console.log(--a);
+// post
+console.log(a++);
+console.log(a--);
+
+// arithmetic-assignment operatoers in C is also used in JS
---
+++ FreeCodeCamp/3. escape sequences.js
@@ -0,0 +1,1 @@
+console.log("It\'s \"Sreelakshmi\'s Partner\"");
