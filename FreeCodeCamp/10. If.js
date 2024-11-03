03e8b1108203520373a2b63c131c1ac7464fcef3
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-11-04 00:08:08 +0530

    comparison, logical, equality, switch

--- FreeCodeCamp/10. If.js
+++ FreeCodeCamp/10. If.js
@@ -4,6 +4,45 @@ function test(val)
     {
         return "Equal";
     }
-    return "Not equal";
+    else if(val > 12)
+    {
+        return "greater"
+    }
+    else
+    {
+        return "lesser";
+    }
 }
 console.log(test(10));
+
+/*
+equality    ==
+strictly equal  === 
+not equal   !=
+strictly not equal  !==
+------------------------------
+comparison 
+
+>   greater than
+>=    greater than or equal to
+<       less than
+<=      less than or  equal to
+-------------------------------
+logical
+
+&&      And
+||      Or
+-------------------------------
+switch
+
+switch(var)
+{
+    case val: stmt;
+        break;
+    case val: stmt;
+        break;
+    default: stmt;
+}
+
+
+*/
