a89dd003ce75db540f38900bb1724961c2f6bdd2
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-02-26 11:03:54 +0530

    select

---
+++ FreeCodeCamp/DOM Manipulation/app.js
@@ -0,0 +1,21 @@
+
+// element by id
+let element = document.getElementById('main-heading');
+console.log(element);
+
+// element by class
+element = document.getElementsByClassName('list-item');
+console.log(element);
+
+// element by tag name
+element = document.getElementsByTagName('li');
+console.log(element);
+
+// element by CSS selectors
+element = document.querySelector('.container');
+console.log(element);
+
+// elements select all
+element = document.querySelectorAll("div"); // all amatches 
+console.log(element);
+
