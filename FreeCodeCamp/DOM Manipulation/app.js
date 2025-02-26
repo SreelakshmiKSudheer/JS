01f65f21151fbd7913c595ec8a9d16de7879b02f
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-02-26 11:34:04 +0530

    changing content

--- FreeCodeCamp/DOM Manipulation/app.js
+++ FreeCodeCamp/DOM Manipulation/app.js
@@ -1,21 +1,30 @@
 
 // element by id
-let element = document.getElementById('main-heading');
-console.log(element);
+// let element = document.getElementById('heading');
+// console.log(element);
 
-// element by class
-element = document.getElementsByClassName('list-item');
-console.log(element);
+// // element by class
+// element = document.getElementsByClassName('list-item');
+// console.log(element);
 
-// element by tag name
-element = document.getElementsByTagName('li');
-console.log(element);
+// // element by tag name
+// element = document.getElementsByTagName('li');
+// console.log(element);
 
-// element by CSS selectors
-element = document.querySelector('.container');
-console.log(element);
+// // element by CSS selectors
+// element = document.querySelector('.container');
+// console.log(element);
 
-// elements select all
-element = document.querySelectorAll("div"); // all amatches 
-console.log(element);
+// // elements select all
+// element = document.querySelectorAll("div"); // all amatches 
+// console.log(element);
+
+let element = document.getElementById("main-heading");
+
+// changing content
+// function innerhtml(){
+function textcontent(){
+    //document.getElementById("main-heading").innerHTML = "Hi welcome again";
+    element.textContent = "Hi there";
+}
 
